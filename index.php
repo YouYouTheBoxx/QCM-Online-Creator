875e515c23ff81ca8733435f7804840ba194120f
YouYouTheBoxx <younes.benmeuraiem7@gmail.com>
2022-03-16 16:13:06 +0100

    Removed Confidiential Informations

--- index.php
+++ index.php
@@ -11,6 +11,6 @@ include('menu.php');
 </head>
 <body>
 <ant>Bienvenue sur QCM-Creator.<br>Ici nous vous proposons de vous créer des applications informatique en C / C++ / C# / VB.NET / Python / Java / QT / Excel VBA sur mesure.<br>Nous vous proposons aussi des services de création de site web stylisés et conforme aux normes W3C sur mesure avec choix et détail du produit final.</ant>
-<ant style="top: 95%"><label style="float: left;">E-Mail: <a href="mailto:contact@formatec33.com">contact@qcmcreator.com</a></label><label>© 2020 Formatec</label></ant>
+<ant style="top: 95%"><label style="float: left;">E-Mail: <a href="mailto:contact@qcmcreator.com">contact@qcmcreator.com</a></label><label>© 2020 Formatec</label></ant>
 </body>
 </html>
