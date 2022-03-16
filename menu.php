d5f2d04a5333aec442fcf3a74387531015b1a684
YouYouTheBoxx <younes.benmeuraiem7@gmail.com>
2022-03-16 16:14:07 +0100

    restructured nav

--- menu.php
+++ menu.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <head>
 <link rel="stylesheet" type="text/css" href="styles/menu.css">
-<ul class="REST">
-<a href="index.php"><li>Home</li></a>
-<a href="mcvirt.php"><li>Ma Classe Virtuelle</li></a>
-</ul>
 </head>
+<header>
+  <nav>
+    <ul class="REST">
+    <a href="index.php"><li>Home</li></a>
+    <a href="mcvirt.php"><li>Ma Classe Virtuelle</li></a>
+    </ul>
+  </nav>
+</header>
 </html>
