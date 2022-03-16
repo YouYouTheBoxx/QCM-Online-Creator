9c6098cd60b05471d9069718a02b068a6ccea28b
YouYouTheBoxx <younes.benmeuraiem7@gmail.com>
2022-03-16 16:12:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include('menu.php');
 </center>
 </head>
 <body>
-<ant>Bienvenue sur Formatec33.<br>Ici nous vous proposons de vous créer des applications informatique en C / C++ / C# / VB.NET / Python / Java / QT / Excel VBA sur mesure.<br>Nous vous proposons aussi des services de création de site web stylisés et conforme aux normes W3C sur mesure avec choix et détail du produit final.</ant>
-<ant style="top: 95%"><label style="float: left;">E-Mail: <a href="mailto:contact@formatec33.com">contact@formatec33.com</a></label><label>© 2020 Formatec</label><label style="float: right;">Numéro de téléphone: 06 21 73 12 88</label></ant>
+<ant>Bienvenue sur QCM-Creator.<br>Ici nous vous proposons de vous créer des applications informatique en C / C++ / C# / VB.NET / Python / Java / QT / Excel VBA sur mesure.<br>Nous vous proposons aussi des services de création de site web stylisés et conforme aux normes W3C sur mesure avec choix et détail du produit final.</ant>
+<ant style="top: 95%"><label style="float: left;">E-Mail: <a href="mailto:contact@formatec33.com">contact@qcmcreator.com</a></label><label>© 2020 Formatec</label></ant>
 </body>
 </html>
